f295f1e155a984a94af04e274cfbace88d83cf8f
Torkut <torkut@protonmail.com>
2021-04-26 00:44:12 +0300

    Create game

---
+++ maps/torkut-nature-single.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="torkut-nature-single" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="../images/montage_tileset.png" width="224" height="192"/>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="8" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="26">
+  <animation>
+   <frame tileid="26" duration="200"/>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="26" duration="200"/>
+   <frame tileid="24" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <animation>
+   <frame tileid="28" duration="200"/>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="30" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <animation>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
